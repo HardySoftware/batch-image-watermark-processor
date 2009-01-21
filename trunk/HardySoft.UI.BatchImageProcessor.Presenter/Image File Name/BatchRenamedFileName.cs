2345be86fb56599a6b10069115e27425af3a00d5
hardy.yin.wang <hardy.yin.wang@a6a698c1-5e45-0410-b0a0-4d9b6f129b3b>
2009-01-21 03:31:32 +0000

    backup code

---
+++ trunk/HardySoft.UI.BatchImageProcessor.Presenter/Image File Name/BatchRenamedFileName.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using HardySoft.UI.BatchImageProcessor.Model;
+using HardySoft.CC;
+
+namespace HardySoft.UI.BatchImageProcessor.Presenter {
+	/// <summary>
+	/// This class is used to generate regaular output file with batch rename enabled.
+	/// </summary>
+	class BatchRenamedFileName : IFilenameProvider {
+		public string GetFileName(string sourceFile, ProjectSetting ps) {
+			string fileNameWithoutExtension, extension, newFileName;
+			FileInfo fi = new FileInfo(sourceFile);
+			string fileName = fi.Name;
+			fileNameWithoutExtension = fileName.Remove(fileName.IndexOf(fi.Extension));
+			extension = fi.Extension;
+
+			if (! string.IsNullOrEmpty(ps.RenamingSetting.OutputFileNamePrefix)
+				|| ! string.IsNullOrEmpty(ps.RenamingSetting.OutputFileNameSuffix)) {
+				newFileName = ps.RenamingSetting.OutputFileNamePrefix 
+					+ (this.imageIndex + ps.RenamingSetting.StartNumber).ToString("D" + ps.RenamingSetting.NumberPadding) 
+					+ ps.RenamingSetting.OutputFileNameSuffix;
+			} else {
+				newFileName = fileNameWithoutExtension + "_"
+					+ (this.imageIndex + ps.RenamingSetting.StartNumber).ToString("D" + ps.RenamingSetting.NumberPadding);
+			}
+			newFileName = newFileName + extension;
+			newFileName = Formatter.FormalizeFolderName(ps.OutputDirectory) + newFileName;
+
+			switch (ps.RenamingSetting.FileNameCase) {
+				case OutputFileNameCase.LowerCase:
+					newFileName = newFileName.ToLower();
+					break;
+				case OutputFileNameCase.UpperCase:
+					newFileName = newFileName.ToUpper();
+					break;
+			}
+			return newFileName;
+		}
+
+		private uint imageIndex;
+		public uint ImageIndex {
+			get { return imageIndex; }
+			set { imageIndex = value; }
+		}
+	}
+}
---
+++ trunk/HardySoft.UI.BatchImageProcessor.Presenter/Image File Name/IFilenameProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HardySoft.UI.BatchImageProcessor.Model;
+
+namespace HardySoft.UI.BatchImageProcessor.Presenter {
+	interface IFilenameProvider {
+		string GetFileName(string sourceFile, ProjectSetting ps);
+		uint ImageIndex {
+			get;
+			set;
+		}
+	}
+}
