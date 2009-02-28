c82264121e8ca1152d36ae13fcfd99329c576328
hardy.yin.wang <hardy.yin.wang@a6a698c1-5e45-0410-b0a0-4d9b6f129b3b>
2009-02-28 16:06:01 +0000

    main control multi-language structure ready

--- branches/2.2/HardySoft.UI.BatchImageProcessor.Model/EnumValues.cs
+++ branches/2.2/HardySoft.UI.BatchImageProcessor.Model/EnumValues.cs
@@ -13,8 +13,13 @@ namespace HardySoft.UI.BatchImageProcessor.Model {
 	}
 
 	public enum OutputFileNameCase {
+		[Description(@"Enum_None")]
 		None,
+
+		[Description(@"Enum_UpperCase")]
 		UpperCase,
+
+		[Description(@"Enum_LowerCase")]
 		LowerCase
 	}
 
@@ -58,8 +63,13 @@ namespace HardySoft.UI.BatchImageProcessor.Model {
 	}
 
 	public enum ShrinkImageMode {
+		[Description(@"Enum_LongSide")]
 		LongSide,
+
+		[Description(@"Enum_Width")]
 		Width,
+
+		[Description(@"Enum_Height")]
 		Height
 	}
 }
