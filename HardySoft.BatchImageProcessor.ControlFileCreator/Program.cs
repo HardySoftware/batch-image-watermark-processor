c35513eb138a9cca511d3b3b64b8b9f2aa5df423
hardy.yin.wang@gmail.com <hardy.yin.wang@gmail.com@a6a698c1-5e45-0410-b0a0-4d9b6f129b3b>
2014-01-04 03:45:35 +0000

    start to add utility to batch create control files.

---
+++ HardySoft.BatchImageProcessor.ControlFileCreator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HardySoft.BatchImageProcessor.ControlFileCreator {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
