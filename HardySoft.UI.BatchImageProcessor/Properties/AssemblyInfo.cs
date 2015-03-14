be14521a809b859c2cb27e1e3d0c0626fc72667c
Hardy Wang <hardywang@outlook.com>
2015-03-14 11:31:40 -0400

    Version update for new release.

--- HardySoft.UI.BatchImageProcessor/Properties/AssemblyInfo.cs
+++ HardySoft.UI.BatchImageProcessor/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.12.*")]
-[assembly: AssemblyFileVersion("2.12.0.0")]
+[assembly: AssemblyVersion("2.14.*")]
+[assembly: AssemblyFileVersion("2.14.0.0")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("HardySoft.UI.BatchImageProcessor.UnitTest")]
